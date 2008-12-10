f4b94cd673987eb60ec2274afc80d88f2ee0a22a
Keolo Keagy <keolo@dreampointmedia.com>
2008-12-09 17:17:28 -0800

    Added some tests where curly quotes don't get converted to &ldquo; and &rdquo;

--- test/rdiscount_test.rb
+++ test/rdiscount_test.rb
@@ -12,4 +12,19 @@ class RDiscountTest < Test::Unit::TestCase
     TEXT
     RDiscount.new(text).to_html
   end
+
+  def test_that_smart_converts_double_quotes_to_curly_quotes
+    rd = RDiscount.new(%("Quoted text"), :smart)
+    assert_equal %(<p>&ldquo;Quoted text&rdquo;</p>\n), rd.to_html
+  end
+
+  def test_that_smart_converts_double_quotes_to_curly_quotes_before_a_heading
+    rd = RDiscount.new(%("Quoted text"\n\n# Heading), :smart)
+    assert_equal %(<p>&ldquo;Quoted text&rdquo;</p>\n\n<h1>Heading</h1>\n), rd.to_html
+  end
+
+  def test_that_smart_converts_double_quotes_to_curly_quotes_after_a_heading
+    rd = RDiscount.new(%(# Heading\n\n"Quoted text"), :smart)
+    assert_equal %(<h1>Heading</h1>\n\n<p>&ldquo;Quoted text&rdquo;</p>\n), rd.to_html
+  end
 end
