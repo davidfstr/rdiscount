b9cb91d7b7215c3196315e63ee4cd1228013bcb7
Austin Ziegler <austin@halostatue.ca>
2010-02-25 20:48:30 -0500

    A cleaner way of making things build on Windows with rubyinstaller and devkit.

--- ext/config.h
+++ ext/config.h
@@ -4,6 +4,17 @@
 #undef USE_AMALLOC
 
 #define TABSTOP 4
+
+#if HAVE_RANDOM
 #define COINTOSS() (random()&1)
+#elif HAVE_RAND
+#define COINTOSS() (rand()&1)
+#endif
+
+#if HAVE_SRANDOM
 #define INITRNG(x) srandom((unsigned int)x)
+#elif HAVE_SRAND
+#define INITRNG(x) srand((unsigned int)x)
+#endif
+
 #define RELAXED_EMPHASIS 1
