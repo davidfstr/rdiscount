48b99782c04817281952912d70b23a343baacb1b
Suraj N. Kurapati <sunaku@gmail.com>
2011-07-13 09:56:54 -0700

    use Discount flags instead of custom attributes

--- ext/rdiscount.c
+++ ext/rdiscount.c
@@ -139,10 +139,36 @@ rb_rdiscount_toc_content(int argc, VALUE *argv, VALUE self)
     return buf;
 }
 
+int rb_rdiscount__get_flags(VALUE ruby_obj)
+{
+    VALUE flags = rb_funcall(ruby_obj, rb_intern("flags"), 0);
+    return NUM2INT(rb_funcall(flags, rb_intern("to_i"), 0));
+}
 
 void Init_rdiscount()
 {
     rb_cRDiscount = rb_define_class("RDiscount", rb_cObject);
+    rb_define_const(rb_cRDiscount, "MKD_NOLINKS", INT2NUM(MKD_NOLINKS));
+    rb_define_const(rb_cRDiscount, "MKD_NOIMAGE", INT2NUM(MKD_NOIMAGE));
+    rb_define_const(rb_cRDiscount, "MKD_NOPANTS", INT2NUM(MKD_NOPANTS));
+    rb_define_const(rb_cRDiscount, "MKD_NOHTML", INT2NUM(MKD_NOHTML));
+    rb_define_const(rb_cRDiscount, "MKD_STRICT", INT2NUM(MKD_STRICT));
+    rb_define_const(rb_cRDiscount, "MKD_TAGTEXT", INT2NUM(MKD_TAGTEXT));
+    rb_define_const(rb_cRDiscount, "MKD_NO_EXT", INT2NUM(MKD_NO_EXT));
+    rb_define_const(rb_cRDiscount, "MKD_CDATA", INT2NUM(MKD_CDATA));
+    rb_define_const(rb_cRDiscount, "MKD_NOSUPERSCRIPT", INT2NUM(MKD_NOSUPERSCRIPT));
+    rb_define_const(rb_cRDiscount, "MKD_NORELAXED", INT2NUM(MKD_NORELAXED));
+    rb_define_const(rb_cRDiscount, "MKD_NOTABLES", INT2NUM(MKD_NOTABLES));
+    rb_define_const(rb_cRDiscount, "MKD_NOSTRIKETHROUGH", INT2NUM(MKD_NOSTRIKETHROUGH));
+    rb_define_const(rb_cRDiscount, "MKD_TOC", INT2NUM(MKD_TOC));
+    rb_define_const(rb_cRDiscount, "MKD_1_COMPAT", INT2NUM(MKD_1_COMPAT));
+    rb_define_const(rb_cRDiscount, "MKD_AUTOLINK", INT2NUM(MKD_AUTOLINK));
+    rb_define_const(rb_cRDiscount, "MKD_SAFELINK", INT2NUM(MKD_SAFELINK));
+    rb_define_const(rb_cRDiscount, "MKD_NOHEADER", INT2NUM(MKD_NOHEADER));
+    rb_define_const(rb_cRDiscount, "MKD_TABSTOP", INT2NUM(MKD_TABSTOP));
+    rb_define_const(rb_cRDiscount, "MKD_NODIVQUOTE", INT2NUM(MKD_NODIVQUOTE));
+    rb_define_const(rb_cRDiscount, "MKD_NOALPHALIST", INT2NUM(MKD_NOALPHALIST));
+    rb_define_const(rb_cRDiscount, "MKD_NODLIST", INT2NUM(MKD_NODLIST));
     rb_define_method(rb_cRDiscount, "to_html", rb_rdiscount_to_html, -1);
     rb_define_method(rb_cRDiscount, "toc_content", rb_rdiscount_toc_content, -1);
 }
--- test/rdiscount_test.rb
+++ test/rdiscount_test.rb
@@ -18,60 +18,60 @@ class RDiscountTest < Test::Unit::TestCase
 
     1.
     TEXT
-    RDiscount.new(text).to_html
+    RDiscount.new(text, :MKD_NOPANTS).to_html
   end
 
   def test_that_smart_converts_double_quotes_to_curly_quotes
-    rd = RDiscount.new(%("Quoted text"), :smart)
+    rd = RDiscount.new(%("Quoted text"))
     assert_equal %(<p>&ldquo;Quoted text&rdquo;</p>\n), rd.to_html
   end
 
   def test_that_smart_converts_double_quotes_to_curly_quotes_before_a_heading
-    rd = RDiscount.new(%("Quoted text"\n\n# Heading), :smart)
+    rd = RDiscount.new(%("Quoted text"\n\n# Heading))
     assert_equal %(<p>&ldquo;Quoted text&rdquo;</p>\n\n<h1>Heading</h1>\n), rd.to_html
   end
 
   def test_that_smart_converts_double_quotes_to_curly_quotes_after_a_heading
-    rd = RDiscount.new(%(# Heading\n\n"Quoted text"), :smart)
+    rd = RDiscount.new(%(# Heading\n\n"Quoted text"))
     assert_equal %(<h1>Heading</h1>\n\n<p>&ldquo;Quoted text&rdquo;</p>\n), rd.to_html
   end
 
   def test_that_smart_gives_ve_suffix_a_rsquo
-    rd = RDiscount.new("I've been meaning to tell you ..", :smart)
+    rd = RDiscount.new("I've been meaning to tell you ..")
     assert_equal "<p>I&rsquo;ve been meaning to tell you ..</p>\n", rd.to_html
   end
 
   def test_that_smart_gives_m_suffix_a_rsquo
-    rd = RDiscount.new("I'm not kidding", :smart)
+    rd = RDiscount.new("I'm not kidding")
     assert_equal "<p>I&rsquo;m not kidding</p>\n", rd.to_html
   end
 
   def test_that_smart_gives_d_suffix_a_rsquo
-    rd = RDiscount.new("what'd you say?", :smart)
+    rd = RDiscount.new("what'd you say?")
     assert_equal "<p>what&rsquo;d you say?</p>\n", rd.to_html
   end
 
   def test_that_generate_toc_sets_toc_ids
-    rd = RDiscount.new("# Level 1\n\n## Level 2", :generate_toc)
-    assert rd.generate_toc
+    rd = RDiscount.new("# Level 1\n\n## Level 2", :MKD_NOPANTS, :MKD_TOC)
+    assert_equal RDiscount::MKD_TOC, rd.flags & RDiscount::MKD_TOC
     assert_equal %(<a name="Level.1"></a>\n<h1>Level 1</h1>\n\n<a name="Level.2"></a>\n<h2>Level 2</h2>\n), rd.to_html
   end
 
   def test_should_get_the_generated_toc
-    rd = RDiscount.new("# Level 1\n\n## Level 2", :generate_toc)
+    rd = RDiscount.new("# Level 1\n\n## Level 2", :MKD_NOPANTS, :MKD_TOC)
     exp = %(<ul>\n <li><a href=\"#Level.1\">Level 1</a>\n <ul>\n  <li><a href=\"#Level.2\">Level 2</a></li>\n </ul>\n </li>\n</ul>)
     assert_equal exp, rd.toc_content.strip
   end
   
   def test_toc_should_escape_apostropes
-    rd = RDiscount.new("# A'B\n\n## C", :generate_toc)
+    rd = RDiscount.new("# A'B\n\n## C", :MKD_NOPANTS, :MKD_TOC)
     exp = %(<ul>\n <li><a href=\"#A.B\">A'B</a>\n <ul>\n  <li><a href=\"#C\">C</a></li>\n </ul>\n </li>\n</ul>)
     assert_equal exp, rd.toc_content.strip
   end
   
   def test_toc_should_escape_question_marks
-    rd = RDiscount.new("# A?B\n\n## C", :generate_toc)
-    exp = %(<ul>\n <li><a href=\"#A.B\">A?B</a>\n <ul>\n  <li><a href=\"#C\">C</a></li>\n </ul>\n </li>\n</ul>)
+    rd = RDiscount.new("# Level 1\n\n## Level 2", :MKD_NOPANTS, :MKD_TOC)
+    exp = %(<ul>\n <li><a href="#Level.1">Level 1</a></li>\n <li><ul>\n  <li><a href="#Level.2">Level 2</a></li>\n </ul></li>\n</ul>)
     assert_equal exp, rd.toc_content.strip
   end
 
@@ -84,17 +84,17 @@ class RDiscountTest < Test::Unit::TestCase
   end
 
   def test_that_no_image_flag_works
-    rd = RDiscount.new(%(![dust mite](http://dust.mite/image.png) <img src="image.png" />), :no_image)
+    rd = RDiscount.new(%(![dust mite](http://dust.mite/image.png) <img src="image.png" />), :MKD_NOPANTS, :MKD_NOIMAGE)
     assert rd.to_html !~ /<img/
   end
 
   def test_that_no_links_flag_works
-    rd = RDiscount.new(%([This link](http://example.net/) <a href="links.html">links</a>), :no_links)
+    rd = RDiscount.new(%([This link](http://example.net/) <a href="links.html">links</a>), :MKD_NOPANTS, :MKD_NOLINKS)
     assert rd.to_html !~ /<a /
   end
 
   def test_that_no_tables_flag_works
-    rd = RDiscount.new(<<EOS, :no_tables)
+    rd = RDiscount.new(<<EOS, :MKD_NOPANTS, :MKD_NOTABLES)
  aaa | bbbb
 -----|------
 hello|sailor
@@ -103,22 +103,22 @@ EOS
   end
 
   def test_that_strict_flag_works
-    rd = RDiscount.new("foo_bar_baz", :strict)
+    rd = RDiscount.new("foo_bar_baz", :MKD_NOPANTS, :MKD_STRICT)
     assert_equal "<p>foo<em>bar</em>baz</p>\n", rd.to_html
   end
 
   def test_that_autolink_flag_works
-    rd = RDiscount.new("http://github.com/davidfstr/rdiscount", :autolink)
+    rd = RDiscount.new("http://github.com/davidfstr/rdiscount", :MKD_NOPANTS, :MKD_AUTOLINK)
     assert_equal "<p><a href=\"http://github.com/davidfstr/rdiscount\">http://github.com/davidfstr/rdiscount</a></p>\n", rd.to_html
   end
 
   def test_that_safelink_flag_works
-    rd = RDiscount.new("[IRC](irc://chat.freenode.org/#freenode)", :safelink)
+    rd = RDiscount.new("[IRC](irc://chat.freenode.org/#freenode)", :MKD_NOPANTS, :MKD_SAFELINK)
     assert_equal "<p>[IRC](irc://chat.freenode.org/#freenode)</p>\n", rd.to_html
   end
 
   def test_that_no_pseudo_protocols_flag_works
-    rd = RDiscount.new("[foo](id:bar)", :no_pseudo_protocols)
+    rd = RDiscount.new("[foo](id:bar)", :MKD_NOPANTS, :MKD_NO_EXT)
     assert_equal "<p>[foo](id:bar)</p>\n", rd.to_html
   end
   
@@ -139,13 +139,7 @@ EOS
   end
 
   def test_that_tags_can_have_dashes_and_underscores
-    if RDiscount::VERSION.start_with? "2.0.7"
-      # Skip test for 2.0.7.x series due to upstream behavioral change in
-      # Discount 2.0.7. This test can be fixed in Discount 2.1.5 using the
-      # WITH_GITHUB_TAGS compile-time flag.
-      return
-    end
-    rd = RDiscount.new("foo <asdf-qwerty>bar</asdf-qwerty> and <a_b>baz</a_b>")
+    rd = RDiscount.new("foo <asdf-qwerty>bar</asdf-qwerty> and <a_b>baz</a_b>", :MKD_NOPANTS)
     assert_equal "<p>foo <asdf-qwerty>bar</asdf-qwerty> and <a_b>baz</a_b></p>\n", rd.to_html
   end
 
